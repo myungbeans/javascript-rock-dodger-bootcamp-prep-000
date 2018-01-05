23f72a1d9f6da5132921a8715ff21582569c7afc
Michael Myung <michael.h.myung@gmail.com>
2018-01-05 03:04:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -80,14 +80,14 @@ function moveDodger(e) {
 }
 
 function moveDodgerLeft() {
-  window.requestAnimationFrame(function() {
-    const left = positionToInteger(DODGER.style.left)
+  const left = positionToInteger(DODGER.style.left)
 
-    if (left > 0) {
-      DODGER.style.left = `${left - 4}px`
-    }
-  })
+  if (left > 0) {
+    DODGER.style.left = `${left - 4}px`
+  }
+  window.requestAnimationFrame(moveDodgerLeft)
 }
+
 
 function moveDodgerRight() {
   window.requestAnimationFrame(function() {
